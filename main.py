45cb014d16630392444ab0f8df475b138130ba2b
Kostov Ilya <kick1991@mail.ru>
2024-05-29 15:19:05 +0500

    add main, update quality_scan.py add ImageQualityAssessor, update utils.py add create_histogram func, add docstrings and annotate

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+import pandas as pd
+from alive_progress import alive_bar
+from quality_scan.quality_scan import ImageCollection, ImageQualityAssessor
+from quality_scan.utils import parse_args, create_histogram
+
+
+def main():
+    args = parse_args()
+    images = ImageCollection(args.images_dir, args.num_processed).find_images()
+    assessor = ImageQualityAssessor(args.sdk_path, args.modification)
+    results = []
+    with alive_bar(len(images)) as progress:
+        for image_path in images:
+            result = assessor.assess_image(image_path)
+            if result:
+                results.append(result)
+                progress()
+
+    df = pd.DataFrame(results)
+
+    output_directory = 'results'
+    os.makedirs(output_directory, exist_ok=True)
+
+    df.to_csv(os.path.join(output_directory, 'results.csv'), index=False)
+    print('Quality assessment completed and save in', output_directory)
+
+    create_histogram(df, output_directory)
+
+
+if __name__ == "__main__":
+    main()
--- quality_scan/utils.py
+++ quality_scan/utils.py
@@ -1,7 +1,14 @@
 import argparse
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command-line arguments for the image quality assessment.
+    :return: parsed command-line arguments
+    """
     parser = argparse.ArgumentParser(description='Calculate quality of images in a directory.')
     parser.add_argument('--images_dir', type=str, required=True, help='Directory containing images.')
     parser.add_argument('--num_processed', type=int, help='Number of images to process.', default=None)
@@ -12,3 +19,11 @@ def parse_args():
     return parser.parse_args()
 
 
+def create_histogram(dataframe: pd.DataFrame, output_directory: str) -> None:
+    """Create a histogram based on the 'totalScore' column of a DataFrame"""
+    dataframe['totalScore'].hist(bins=10)
+    plt.xlabel('Total Scores')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Total Scores')
+    plt.savefig(os.path.join(output_directory, 'total_score_histogram.png'))
+    print('Histogram successfully created and saved in', output_directory)
